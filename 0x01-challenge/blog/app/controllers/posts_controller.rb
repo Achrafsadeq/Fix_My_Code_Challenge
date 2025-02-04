0ef15aeceead0139d9aaa2e4a6711a4e6abd9b09
Achrafsadeq <achrafsadeqq@gmail.com>
2025-02-04 09:14:15 -0800

    Fix a Rails blog application and add a new feature for post visibility.

--- 0x01-challenge/blog/app/controllers/posts_controller.rb
+++ 0x01-challenge/blog/app/controllers/posts_controller.rb
@@ -1,51 +1,66 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-  
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
   def index
-    @posts = Post.all.order('created_at DESC')
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      redirect_to @post
+    if user_signed_in?
+      @posts = Post.all.order(created_at: :desc)
     else
-      render 'new'
+      @posts = Post.where(online: true).order(created_at: :desc)
     end
   end
 
   def show
-    @post = Post.find(params[:id])
+    if !@post.online? && @post.user != current_user
+      redirect_to root_path, alert: "This post is not available."
+    end
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    if @post.update(params[:post].permit(:title, :body))
-      redirect_to @post
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
     else
-      render 'edit'
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-
-    redirect_to root_path
+    redirect_to root_path, notice: 'Post was successfully deleted.'
   end
 
   private
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+
+  def set_post
+    @post = Post.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: 'Post not found.'
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :online) # Added :online
+  end
+
+  def authorize_user!
+    redirect_to root_path, alert: 'You are not authorized to perform this action.' unless @post.user == current_user
+  end
 end
+
